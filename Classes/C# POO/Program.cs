ca35100e6f6c96f0bc00a5781947c0b4750c49e7
enzolas <enzo.quarelo@gmail.com>
2023-04-20 10:39:46 -0300

    test

--- Classes/C# POO/Program.cs
+++ Classes/C# POO/Program.cs
@@ -1,14 +1,27 @@
 ﻿using C__POO;
 
-Personagem tony = new Personagem();
+Personagem Personagem1 = new Personagem();
 
-Console.WriteLine(tony.nome);
-Console.WriteLine(tony.idade);
-Console.WriteLine(tony.armadura);
-Console.WriteLine(tony.ia);
+Console.WriteLine($"Informe o nome do seu campeao :");
+Personagem1.nome = Console.ReadLine();
+Console.WriteLine($"Informe a idade de {Personagem1.nome} :");
+Personagem1.idade = int.Parse(Console.ReadLine());
+Console.WriteLine($"O {Personagem1.nome} tera armadura ? Se sim informe o nome de sua armadura:");
+Personagem1.armadura = Console.ReadLine();
+Console.WriteLine($"Informe a ia (inteligencia artificial) utilizada pelo {Personagem1.nome}:");
+Personagem1.ia = Console.ReadLine();
 
-tony.Atacar();
 
-Console.WriteLine(tony.Defender());
+Console.WriteLine($"Confira as informacoes do seu campeao");
+Console.WriteLine(@$"
+Nome : {Personagem1.nome}
+Idade : {Personagem1.idade}
+Armadura : {Personagem1.armadura}
+I.A : {Personagem1.ia}");
 
-tony.RestaurarArmadura();
+Personagem1.Atacar();
+Personagem1.RestaurarArmadura();
+Console.WriteLine(Personagem1.Defender());
+
+
+
