42392d15bfc83d16c0272fe837b77b3e415e9c85
enzolas <enzo.quarelo@gmail.com>
2023-04-24 11:03:51 -0300

    test

---
+++ Exercises/24-04-2023 C# Classes/Students/Aluno.cs
@@ -0,0 +1,55 @@
+//Crie uma classe Aluno com as seguintes propriedades:
+
+//Nome, Curso, Idade, RG, Bolsista (boolean), Média Final e Valor da Mensalidade. 
+//Também teremos os métodos: 
+
+//VerMediaFinal() e VerMensalidade(), caso seja bolsista faremos o cálculo da mensalidade.
+
+//obs:
+//bolsista e média final maior ou igual a 8 conceder 50% de desconto na mensalidade
+//bolsista e média final maior que 6 e menor que 8 conceder 30% de desconto na mensalidade 
+//outros casos a mensalidade será integral
+//Receba os dados do cadastro via console e crie um menu para o usuário escolher se quer visualizar a média ou o valor da mensalidade.
+
+namespace Students
+{
+    public class Aluno
+    {
+        public string Nome ;
+        public string Curso;
+        public int RG;
+        public bool Bolsista;
+        public float MediaFinal;
+        public float Mensalidade;
+
+        public void VerMediaFinal()
+        {
+            Console.WriteLine($"A media final de {Nome} e de {MediaFinal}.");
+              
+        }
+        public float VerMensalidade()
+        {
+            float MensalidadeFinal;
+
+            if(Bolsista == true && MediaFinal >= 8)
+            {
+
+                MensalidadeFinal = Mensalidade - (50/100 * Mensalidade);
+                    
+            }
+            else if(Bolsista == true && MediaFinal > 6 && MediaFinal < 8)
+            {
+
+                MensalidadeFinal = Mensalidade - (30/100 * Mensalidade);
+            
+                    
+            }
+            else
+            {
+                MensalidadeFinal = Mensalidade;
+                
+            }
+            return MensalidadeFinal;
+        }
+    }
+}
---
+++ Exercises/24-04-2023 C# Classes/Students/Program.cs
@@ -0,0 +1,68 @@
+﻿using Students;
+
+Aluno Aluno= new Aluno();
+
+Console.WriteLine($"Bem-vindo ao Programa de cadastro de alunos : *Precione <Enter> para seguir*");
+Console.ReadLine();
+
+Console.WriteLine($"Informe o nome do aluno :");
+Aluno.Nome = Console.ReadLine();
+Console.WriteLine($"Informe o curso que {Aluno.Nome} esta cursando :");
+Aluno.Curso = Console.ReadLine();
+Console.WriteLine($"Informe o RG de {Aluno.Nome} :");
+Aluno.RG = int.Parse(Console.ReadLine());
+Console.WriteLine($"O(a) {Aluno.Nome} e bolsista ? s/n");
+char bolsistaResposta = char.Parse(Console.ReadLine().ToLower());
+if(bolsistaResposta == 's')
+{
+    bool Bolsista = true;
+}
+else if(bolsistaResposta == 'n')
+{
+    bool Bolsista = false;
+}
+else
+{
+    Console.WriteLine($"Por-favor insira uma resposta valida. s/n");
+}
+Console.WriteLine($"Informe a media final de {Aluno.Nome} :");
+Aluno.MediaFinal = float.Parse(Console.ReadLine());
+Console.WriteLine($"Informe a mensalidade a ser paga por {Aluno.Nome} :");
+Aluno.Mensalidade = float.Parse(Console.ReadLine());
+
+char opcaoMenu;
+
+do
+{
+    Console.WriteLine(@$"
+ __________________________
+|        Menu Inicial      |
+|1- Visualizar Media Final |
+|2- Visulizar Mensalidade  |
+|                          |
+|0- Sair                   |
+|__________________________|");
+Console.WriteLine($"");
+
+
+opcaoMenu = char.Parse(Console.ReadLine());
+
+switch(opcaoMenu)
+{
+    case '1':
+        Aluno.VerMediaFinal();
+    break;
+    case '2':
+        Console.WriteLine($"A mensalidade de {Aluno.Nome} a ser paga e de: R${Aluno.VerMensalidade()}");
+    break;
+    case '0':
+    Console.WriteLine($"Obrigado por utilizar nosso programa !");
+    break;
+
+}
+
+} while (opcaoMenu != 0);
+
+
+
+
