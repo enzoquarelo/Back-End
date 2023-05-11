a1385a54a5e3f286f2705f542b844e197ef5bbe3
enzolas <enzo.quarelo@gmail.com>
2023-05-11 08:38:37 -0300

    correcao exercicio

--- Exercises/10-05-2023 Lists/Extra Exercise/Program.cs
+++ Exercises/10-05-2023 Lists/Extra Exercise/Program.cs
@@ -12,17 +12,24 @@
 
 using Extra_Exercise;
 
+Carro carro = new Carro();
 List<Carro> listCarro = new List<Carro>();
 
-listCarro.Add(new Carro("BMW","Vermelho"));
-listCarro.Add(new Carro("Fiat","Cinza"));
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Cadastro do {i + 1}º carro:");
+    Console.Write("Marca: ");
+    string marca = Console.ReadLine();
+    Console.Write("Cor: ");
+    string cor = Console.ReadLine();
 
+    listCarro.Add(new Carro(marca, cor));
+}
+
+Console.WriteLine("Dados dos carro cadastrados:");
 foreach (var item in listCarro)
 {
     Console.WriteLine(@$"
-    Produto Carro:
     Marca: {item.Marca}
-    Cor: {item.Cor}
-    ");
-    
+    Cor: {item.Cor}");
 }
